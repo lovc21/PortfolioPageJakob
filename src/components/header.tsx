c62d72706528f28597c00956c21c41f2acd0a604
lovc21 <jakob.dekleva@gmail.com>
2023-08-21 17:10:43 +0200

    fix footer image and fix text to work in dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,21 +4,22 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className="text-gray-600 body-font">
+        <header className="text-gray-600 body-font bg-background-primary">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <a className="flex title-font font-medium items-center text-primary mb-4 md:mb-0">
                     <Image src="/myLogo.svg" alt="Logo" width={40} height={40} className="text-white p-2 bg-indigo-500 rounded-full" />
                     <span className="ml-3 text-xl">Jakob Dekleva</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">About me</a>
-                    <a className="mr-5 hover:text-gray-900">Gallery</a>
-                    <a className="mr-5 hover:text-gray-900">Projects</a>
-                    <a className="mr-5 hover:text-gray-900">Something cool</a>
+                    <a className="mr-5 hover:text-primary">About me</a>
+                    <a className="mr-5 hover:text-primary">Gallery</a>
+                    <a className="mr-5 hover:text-primary">Projects</a>
+                    <a className="mr-5 hover:text-primary">Something cool</a>
                     <ThemeButten/>
                 </nav>
             </div>
         </header>
     );
 }
+
 export default Header;
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,16 +4,19 @@ import Image from 'next/image';
 
 const Hero = () => {
     return (
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font bg-background-primary">
             <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
                 <Image src="/ProfilPhoto.png" alt="Profil" width={500} height={500} className="mb-10 object-cover object-center rounded" />
                 <div className="text-center lg:w-2/3 w-full">
-                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                            Welcome to Jakob&apos;s webpage <span className="waving-hand">👋</span>.
-                        </h1>
-                        <p className="mb-8 leading-relaxed">Hi, my name is Jakob, and this is my webpage. On this page, you can find a little bit about me, some projects I did, and a photo gallery of cool places I traveled to.</p>
-                        <p className="mb-8 leading-relaxed">Enjoy your stay and please don&apos;t hack me.</p>
-                    </div>
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-primary">
+                        Welcome to Jakob's webpage <span className="waving-hand">👋</span> .
+                    </h1>
+                    <p className="mb-8 leading-relaxed text-primary">Hi, my name is Jakob Dekleva, and this is my webpage. On this page, you can find a little bit about me, some projects I did, and a photo gallery of cool places I traveled to.</p>
+                    <p className="mb-8 leading-relaxed text-primary">Enjoy your stay and please don't hack me.</p>
+                </div>
+                <footer className="w-full text-center mt-10 text-primary">
+                    Jakob Dekleva
+                </footer>
             </div>
         </section>
     );
